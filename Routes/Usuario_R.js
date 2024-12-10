9d7c76c759259fad2da20b952b1c5ce20fe991b6
Mauricio Quiroga <mauriquiroga97@gmail.com>
2024-12-10 17:06:18 -0300

    rename index->login,fix/server.js navbar.hbs

--- Routes/Usuario_R.js
+++ Routes/Usuario_R.js
@@ -1,244 +1,267 @@
-const axios = require('axios');
-const { response } = require('express');
-const fs = require('fs');
-const Handlebars = require('hbs');
+const axios = require("axios");
+const { response } = require("express");
+const fs = require("fs");
+const Handlebars = require("hbs");
 
 const inicio = (req, res) => {
-    res.render("./index", {});
-}
+  res.render("./index", {});
+};
 // _____________sobre nosotros----------
 const sobreNosotros = (req, res) => {
-    res.render('./sobreNosotros', {});
-}
+  res.render("./sobreNosotros", {});
+};
 
 const menu = (req, res) => {
-    console.log("Datos recibidos del cliente:", req.body);
-    axios.post("http://localhost:3333/login", {
-        email: req.body.email,
-        contrasena: req.body.contrasena
+  console.log("Datos recibidos del cliente:", req.body);
+  axios
+    .post("http://localhost:3333/login", {
+      email: req.body.email,
+      contrasena: req.body.contrasena,
     })
-        .then(response => {
-            if (response.status === 200) {
-                const idUsuario = response.data.usuarioAutorizado.id_usuario;
-                const rol = response.data.usuarioAutorizado.rol;
-                console.log("Usuario autenticado con id:", idUsuario, "--rol:  ", rol);
+    .then((response) => {
+      if (response.status === 200) {
+        const idUsuario = response.data.usuarioAutorizado.id_usuario;
+        const rol = response.data.usuarioAutorizado.rol;
+        console.log("Usuario autenticado con id:", idUsuario, "--rol:  ", rol);
 
-                const nombre = response.data.usuarioAutorizado.nombre;
-                const email = response.data.usuarioAutorizado.email;
-                const telefono = response.data.usuarioAutorizado.telefono;
+        const nombre = response.data.usuarioAutorizado.nombre;
+        const email = response.data.usuarioAutorizado.email;
+        const telefono = response.data.usuarioAutorizado.telefono;
 
-                // Establecer la cookie idUsuario con un tiempo de expiración de 1 día
-                res.cookie("idUsuario", idUsuario, { maxAge: 24 * 60 * 60 * 1000, httpOnly: true });
-                res.cookie("rol", rol, { maxAge: 24 * 60 * 60 * 1000, httpOnly: true });
-                res.cookie
-                res.cookie("nombre", nombre)
-                res.cookie("email", email)
-                res.cookie("telefono", telefono)
-
-                // Renderizar plantilla basada en el rol
-                if (rol == "false") {
-                    res.render("./comercio/menuComercio", {});
-                } else {
-                    res.redirect("/listarUsuarios"); 
-                }
-            } else {
-                res.status(401).send("Credenciales inválidas o falta idUsuario");
-            }
-        })
-        .catch(error => {
-            console.error("Error en el login:", error);
-            res.status(500).send("Error en el login");
+        // Establecer cookies
+        res.cookie("idUsuario", idUsuario, {
+          maxAge: 24 * 60 * 60 * 1000,
+          httpOnly: true,
         });
-}
+        res.cookie("rol", rol, { maxAge: 24 * 60 * 60 * 1000, httpOnly: true });
+        res.cookie("nombre", nombre);
+        res.cookie("email", email);
+        res.cookie("telefono", telefono);
+
+        // Renderizar plantilla basada en el rol
+        if (rol == "false" || !rol) {
+          res.render("./comercio/menuComercio", {});
+        } else {
+          res.redirect("/listarUsuarios");
+        }
+      } else {
+        res.status(401).send("Credenciales inválidas o falta idUsuario");
+      }
+    })
+    .catch((error) => {
+      console.log("Error en autenticación:", error.message);
+      res.render("login", {
+        error: "Credenciales inválidas, por favor verifica tus datos",
+      });
+    });
+};
+
 //perfil
 const perfil = (req, res) => {
-    // Leer las cookies
-    const idUsuario = req.cookies.idUsuario;
-    const rol = req.cookies.rol;
-    const nombre = req.cookies.nombre;
-    const email = req.cookies.email;
-    const telefono = req.cookies.telefono;
+  // Leer las cookies
+  const idUsuario = req.cookies.idUsuario;
+  const rol = req.cookies.rol;
+  const nombre = req.cookies.nombre;
+  const email = req.cookies.email;
+  const telefono = req.cookies.telefono;
 
-    console.log("Cookies recibidas:");
-    console.log("ID Usuario:", idUsuario);
-    console.log("Rol:", rol);
-    console.log("Nombre:", nombre);
-    console.log("Email:", email);
-    console.log("Teléfono:", telefono);
+  console.log("Cookies recibidas:");
+  console.log("ID Usuario:", idUsuario);
+  console.log("Rol:", rol);
+  console.log("Nombre:", nombre);
+  console.log("Email:", email);
+  console.log("Teléfono:", telefono);
 
-    axios.get(`http://localhost:3333/comercio/owner/${idUsuario}`)
-        .then(response => {
-            const comercios = response.data.comercio;
+  axios
+    .get(`http://localhost:3333/comercio/owner/${idUsuario}`)
+    .then((response) => {
+      const comercios = response.data.comercio;
 
-            console.log("Comercios relacionados al usuario:");
-            console.log(comercios);
+      console.log("Comercios relacionados al usuario:");
+      console.log(comercios);
 
-            res.render("users/perfil", { 
-                nombre, 
-                email, 
-                telefono, 
-                idUsuario,
-                comercios // Pasar la lista de comercios a la vista
-            });
-        })
-        .catch(error => {
-            console.error('Error al obtener usuarios con comercios:', error);
-            res.status(500).send('Error al obtener usuarios');
-        });
+      res.render("users/perfil", {
+        nombre,
+        email,
+        telefono,
+        idUsuario,
+        comercios, // Pasar la lista de comercios a la vista
+      });
+    })
+    .catch((error) => {
+      console.error("Error al obtener usuarios con comercios:", error);
+      res.status(500).send("Error al obtener usuarios");
+    });
 };
 
 // ----------listar usuarios----------
 const listarUsuarios = (req, res) => {
-    axios.get('http://localhost:3333/usuario')
-        .then(response => {
-            const usuariosRegistrados = response.data.usuariosRegistrados;
-            res.render("administrador/listarUsuarios", { usuarios: usuariosRegistrados });
-        })
-        .catch(error => {
-            console.error('Error al obtener usuarios:', error);
-            res.status(500).send('Error al obtener usuarios');
-        });
+  axios
+    .get("http://localhost:3333/usuario")
+    .then((response) => {
+      const usuariosRegistrados = response.data.usuariosRegistrados;
+      res.render("administrador/listarUsuarios", {
+        usuarios: usuariosRegistrados,
+      });
+    })
+    .catch((error) => {
+      console.error("Error al obtener usuarios:", error);
+      res.status(500).send("Error al obtener usuarios");
+    });
 };
 // registrar usuario
 const formRegistrarUsuario = (req, res) => {
-    res.render("./agregarUsuario", {});
-}
+  res.render("./agregarUsuario", {});
+};
 const registrarUsuario = (req, res) => {
-    axios.post('http://localhost:3333/usuario/registrar', {
-        nombre: req.body.nombre,
-        email: req.body.email,
-        telefono: req.body.telefono,
-        contrasena: req.body.pass,
-        rol: req.body.rol
+  axios
+    .post("http://localhost:3333/usuario/registrar", {
+      nombre: req.body.nombre,
+      email: req.body.email,
+      telefono: req.body.telefono,
+      contrasena: req.body.pass,
+      rol: req.body.rol,
     })
-        .then(response => {
-            if (response.status === 201) {
-                res.render("./index", {});
-            } else {
-                console.log("Error en los datos ingresados");
-            }
-        })
-        .catch(error => {
-            console.error('Error al registrar usuario:', error);
-            res.status(500).send('Error al registrar usuario');
-        });
+    .then((response) => {
+      if (response.status === 201) {
+        res.render("./index", {});
+      } else {
+        console.log("Error en los datos ingresados");
+      }
+    })
+    .catch((error) => {
+      console.error("Error al registrar usuario:", error);
+      res.status(500).send("Error al registrar usuario");
+    });
 };
 //menu
 const home = (req, res) => {
-    res.render("./menu", {});
-}
+  res.render("./menu", {});
+};
 //registrar administrador
 const formRegistrarAdministrador = (req, res) => {
-    res.render("administrador/agregarAdmi", {});
-} 
+  res.render("administrador/agregarAdmi", {});
+};
 const registrarAdministrador = (req, res) => {
-    console.log(req.body.nombre);
-    axios.post('http://localhost:3333/usuario/registrar', {
-        nombre: req.body.nombre,
-        email: req.body.email,
-        telefono: req.body.telefono,
-        contrasena: req.body.pass,
-        rol: req.body.rol
+  console.log(req.body.nombre);
+  axios
+    .post("http://localhost:3333/usuario/registrar", {
+      nombre: req.body.nombre,
+      email: req.body.email,
+      telefono: req.body.telefono,
+      contrasena: req.body.pass,
+      rol: req.body.rol,
     })
-        .then(response => {
-            if (response.status === 201) {
-                if (req.cookies.rol === true) {
-                    res.redirect("/listarUsuarios");
-                } else {
-                    res.render("./agregarUsuario", {});
-                }
-            } else {
-                console.log("server <-r- backend 'error en los datos ingresados'");
-            }
-        })
-}
+    .then((response) => {
+      if (response.status === 201) {
+        if (req.cookies.rol === true) {
+          res.redirect("/listarUsuarios");
+        } else {
+          res.render("./agregarUsuario", {});
+        }
+      } else {
+        console.log("server <-r- backend 'error en los datos ingresados'");
+      }
+    });
+};
 // Eliminar usuario/administrador
 const eliminarUsuario = (req, res) => {
-    const idUsuario = req.params.idUsuario; // Obtener ID desde la ruta
-    console.log("ID del usuario a eliminar:", idUsuario);
+  const idUsuario = req.params.idUsuario; // Obtener ID desde la ruta
+  console.log("ID del usuario a eliminar:", idUsuario);
 
-    axios.delete(`http://localhost:3333/usuario/${idUsuario}`)
-        .then(() => {
-            console.log("Usuario eliminado correctamente");
-            res.redirect('/listarUsuarios');
-        })
-        .catch((error) => {
-            console.error("Error al eliminar el usuario:", error.response ? error.response.data : error.message);
-            res.status(500).render('administrador/listarUsuarios', {
-                error: "No se pudo eliminar el usuario. Intenta nuevamente.",
-            });
-        });
+  axios
+    .delete(`http://localhost:3333/usuario/${idUsuario}`)
+    .then(() => {
+      console.log("Usuario eliminado correctamente");
+      res.redirect("/listarUsuarios");
+    })
+    .catch((error) => {
+      console.error(
+        "Error al eliminar el usuario:",
+        error.response ? error.response.data : error.message
+      );
+      res.status(500).render("administrador/listarUsuarios", {
+        error: "No se pudo eliminar el usuario. Intenta nuevamente.",
+      });
+    });
 };
 // modificar usuario
 
 // Ruta para obtener los datos del usuario por id y mostrar el formulario de edición
 const formModificarUsuario = (req, res) => {
-    const idUsuarioAModificar = req.params.id_usuario;
-    console.log("el id a modificar es: ", idUsuarioAModificar);
+  const idUsuarioAModificar = req.params.id_usuario;
+  console.log("el id a modificar es: ", idUsuarioAModificar);
 
-    // res.cookie("idCambio", idUsuarioAModificar, { maxAge: 24 * 60 * 60 * 1000, httpOnly: true });
+  // res.cookie("idCambio", idUsuarioAModificar, { maxAge: 24 * 60 * 60 * 1000, httpOnly: true });
 
-    // Solicitar los datos del usuario desde el servidor
-    axios.get(`http://localhost:3333/usuario/${idUsuarioAModificar}`, { withCredentials: true })
-        .then(response => {
-            console.log(response.data.user)
-            const usuario = response.data.user;
-            res.render("administrador/editarUsuario", { usuario: usuario });
-        })
-        .catch(error => {
-            console.error("Error al obtener el usuario:", error);
-            res.status(500).send("Error al obtener el usuario");
-        });
+  // Solicitar los datos del usuario desde el servidor
+  axios
+    .get(`http://localhost:3333/usuario/${idUsuarioAModificar}`, {
+      withCredentials: true,
+    })
+    .then((response) => {
+      console.log(response.data.user);
+      const usuario = response.data.user;
+      res.render("administrador/editarUsuario", { usuario: usuario });
+    })
+    .catch((error) => {
+      console.error("Error al obtener el usuario:", error);
+      res.status(500).send("Error al obtener el usuario");
+    });
 };
 
 // Función para manejar la modificación del usuario
 const modificarUsuario = async (req, res) => {
-    const id_usuario = req.params.id;
-    const { nombre, email, telefono, rol } = req.body;
+  const id_usuario = req.params.id;
+  const { nombre, email, telefono, rol } = req.body;
 
-    const usuarioModificado = {
-        nombre: req.body.nombre,
-        email: req.body.email,
-        telefono: req.body.telefono,
-        rol: rol === "true" ? true : false
-    };
-    console.log(usuarioModificado);
+  const usuarioModificado = {
+    nombre: req.body.nombre,
+    email: req.body.email,
+    telefono: req.body.telefono,
+    rol: rol === "true" ? true : false,
+  };
+  console.log(usuarioModificado);
 
-    axios.put(`http://localhost:3333/usuario/editar/${id_usuario}`, usuarioModificado )
-        .then(response => {
-            if (response.status === 200) {
-                res.redirect("/listarUsuarios"); // Redirigir a la lista de usuarios después de la actualización
-            } else {
-                res.status(500).send("Error al actualizar el usuario");
-            }
-        })
-        .catch(error => {
-            console.error('Error al actualizar el usuario:', error);
-            res.status(500).send('Error al actualizar el usuario');
-        });
-}
+  axios
+    .put(
+      `http://localhost:3333/usuario/editar/${id_usuario}`,
+      usuarioModificado
+    )
+    .then((response) => {
+      if (response.status === 200) {
+        res.redirect("/listarUsuarios"); // Redirigir a la lista de usuarios después de la actualización
+      } else {
+        res.status(500).send("Error al actualizar el usuario");
+      }
+    })
+    .catch((error) => {
+      console.error("Error al actualizar el usuario:", error);
+      res.status(500).send("Error al actualizar el usuario");
+    });
+};
 
 // cerrar sesion
 const logout = (req, res) => {
-    res.clearCookie("idUsuario");
-    res.clearCookie("rol");
-    res.redirect("/");
-}
+  res.clearCookie("idUsuario");
+  res.clearCookie("rol");
+  res.redirect("/");
+};
 
 module.exports = {
-    inicio,
-    menu,
-    sobreNosotros,
-    formRegistrarAdministrador,
-    formModificarUsuario,
-    modificarUsuario,
-    registrarAdministrador,
-    listarUsuarios,
-    // obtenerUsuario,
-    formRegistrarUsuario,
-    registrarUsuario,
-    eliminarUsuario,
-    logout, 
-    perfil, 
-    home
+  inicio,
+  menu,
+  sobreNosotros,
+  formRegistrarAdministrador,
+  formModificarUsuario,
+  modificarUsuario,
+  registrarAdministrador,
+  listarUsuarios,
+  // obtenerUsuario,
+  formRegistrarUsuario,
+  registrarUsuario,
+  eliminarUsuario,
+  logout,
+  perfil,
+  home,
 };
--- server.js
+++ server.js
@@ -1,89 +1,158 @@
 const express = require("express");
-const path = require('path');
-const hbs = require('hbs');
+const path = require("path");
+const hbs = require("hbs");
 const methodOverride = require("method-override");
-const fs = require('fs');
+const fs = require("fs");
 const axios = require("axios");
 
 const app = express();
 const port = 3001;
-const upload = require('./middlewares/upload');
+const upload = require("./middlewares/upload");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'views'));
-hbs.registerPartials(path.join(__dirname, 'views/partials'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
+hbs.registerPartials(path.join(__dirname, "views/partials"));
+app.use(express.static(path.join(__dirname, "public")));
 
 const cookieParser = require("cookie-parser");
-app.use(cookieParser()); 
+app.use(cookieParser());
 
-const { admin } = require('./middlewares/authenticaded');
-const { logueado } = require('./middlewares/authenticaded');
-const usuarioRoutes = require('./Routes/Usuario_R');
-const comercioRoutes = require('./Routes/Comercio_R');
-const productoRoutes = require('./Routes/Producto_R')
-const categoriaRoutes = require('./Routes/Categoria_R')
+const { admin } = require("./middlewares/authenticaded");
+const { logueado } = require("./middlewares/authenticaded");
+const usuarioRoutes = require("./Routes/Usuario_R");
+const comercioRoutes = require("./Routes/Comercio_R");
+const productoRoutes = require("./Routes/Producto_R");
+const categoriaRoutes = require("./Routes/Categoria_R");
 
 //rutas del inicio----------
 app.get("/", (req, res) => {
-    res.render("./inicio", {});
-  });
-  app.get("/login", (req, res)=>{
-      res.render("index.hbs", {});
-  } )
-  app.get("/comercios", (req, res) => {
-    res.render("./listadoComercios", {});
-  });
-  app.get("/nosotros", (req, res) => {
-    res.render("./sobreNosotros", {});
-  });
- 
+  res.render("inicio", {});
+});
+app.get("/login", (req, res) => {
+  res.render("login", {});
+});
+
+app.get("/comercios", (req, res) => {
+  res.render("./listadoComercios", {});
+});
+app.get("/nosotros", (req, res) => {
+  res.render("./sobreNosotros", {});
+});
+
 // Rutas relacionadas a usuarios
-app.post('/menu', usuarioRoutes.menu);
-app.get('/menu',  usuarioRoutes.home);
-app.get('/registrarUsuario', usuarioRoutes.formRegistrarUsuario);
-app.post('/registrarUsuario', usuarioRoutes.registrarUsuario);
-app.get('/sobreNosotros', usuarioRoutes.sobreNosotros );
+app.post("/menu", usuarioRoutes.menu);
+app.get("/menu", usuarioRoutes.home);
+app.get("/registrarUsuario", usuarioRoutes.formRegistrarUsuario);
+app.post("/registrarUsuario", usuarioRoutes.registrarUsuario);
+app.get("/sobreNosotros", usuarioRoutes.sobreNosotros);
 app.get("/perfil", logueado, usuarioRoutes.perfil);
 app.get("/tusProductos", logueado, comercioRoutes.prodComercio);
- 
+
 // administrador
-app.get('/listarUsuarios', logueado, admin, usuarioRoutes.listarUsuarios);
-app.get('/listarComercios', logueado, admin, comercioRoutes.listarComercios);
-app.get('/registrarAdministrador', logueado, admin, usuarioRoutes.formRegistrarAdministrador);
-app.post('/registrarAdministrador', logueado, admin, usuarioRoutes.registrarAdministrador);
-app.delete('/eliminarUsuario/:idUsuario', logueado, admin, usuarioRoutes.eliminarUsuario);
-app.delete('/eliminarComercio/:idComercio', logueado, admin, comercioRoutes.eliminarComercio);
-app.get('/editarUsuario/:id_usuario', logueado, admin, usuarioRoutes.formModificarUsuario);
-app.post('/usuario/editar/:id', logueado, admin, usuarioRoutes.modificarUsuario);
-   
+app.get("/listarUsuarios", logueado, admin, usuarioRoutes.listarUsuarios);
+app.get("/listarComercios", logueado, admin, comercioRoutes.listarComercios);
+app.get(
+  "/registrarAdministrador",
+  logueado,
+  admin,
+  usuarioRoutes.formRegistrarAdministrador
+);
+app.post(
+  "/registrarAdministrador",
+  logueado,
+  admin,
+  usuarioRoutes.registrarAdministrador
+);
+app.delete(
+  "/eliminarUsuario/:idUsuario",
+  logueado,
+  admin,
+  usuarioRoutes.eliminarUsuario
+);
+app.delete(
+  "/eliminarComercio/:idComercio",
+  logueado,
+  admin,
+  comercioRoutes.eliminarComercio
+);
+app.get(
+  "/editarUsuario/:id_usuario",
+  logueado,
+  admin,
+  usuarioRoutes.formModificarUsuario
+);
+app.post(
+  "/usuario/editar/:id",
+  logueado,
+  admin,
+  usuarioRoutes.modificarUsuario
+);
+
 // categorias(solo accede el administrador)
-app.get('/listarCategorias', logueado, admin, categoriaRoutes.listarCategorias);
-app.get('/registrarCategoria', logueado, admin, categoriaRoutes.formAgregarCategoria);
-app.post('/registrarCategoria', logueado, admin, categoriaRoutes.agregarCategoria);
-app.delete('/eliminarCategoria/:idCategoria', logueado, admin, categoriaRoutes.eliminarCategoria);
-app.get('/editarCategoria/:idCategoria', logueado, admin, categoriaRoutes.formModificarCategoria);
-app.post('/categoria/editar/:idCategoria', logueado, admin, categoriaRoutes.modificarCategoria);
-    
-//producto 
-app.get('/agregarProducto', logueado, productoRoutes.formAgregarProducto)
-app.post('/agregarProducto', logueado, upload.single('imgProducto'), productoRoutes.agregarProducto)
+app.get("/listarCategorias", logueado, admin, categoriaRoutes.listarCategorias);
+app.get(
+  "/registrarCategoria",
+  logueado,
+  admin,
+  categoriaRoutes.formAgregarCategoria
+);
+app.post(
+  "/registrarCategoria",
+  logueado,
+  admin,
+  categoriaRoutes.agregarCategoria
+);
+app.delete(
+  "/eliminarCategoria/:idCategoria",
+  logueado,
+  admin,
+  categoriaRoutes.eliminarCategoria
+);
+app.get(
+  "/editarCategoria/:idCategoria",
+  logueado,
+  admin,
+  categoriaRoutes.formModificarCategoria
+);
+app.post(
+  "/categoria/editar/:idCategoria",
+  logueado,
+  admin,
+  categoriaRoutes.modificarCategoria
+);
+
+//producto
+app.get("/agregarProducto", logueado, productoRoutes.formAgregarProducto);
+app.post(
+  "/agregarProducto",
+  logueado,
+  upload.single("imgProducto"),
+  productoRoutes.agregarProducto
+);
 app.get("/listarProductos", logueado, productoRoutes.listarProductos);
 app.delete("/eliminarProducto/:id_producto", productoRoutes.eliminarProducto);
-app.get("/editarProducto/:idProducto", logueado, productoRoutes.formEditarProducto);
-app.post("/producto/editar/:idProducto", logueado, productoRoutes.modificarProducto);
- 
+app.get(
+  "/editarProducto/:idProducto",
+  logueado,
+  productoRoutes.formEditarProducto
+);
+app.post(
+  "/producto/editar/:idProducto",
+  logueado,
+  productoRoutes.modificarProducto
+);
+
 // comercio
-app.get('/agregarComercio', logueado, comercioRoutes.formAgregarComercio);
-app.post('/agregarComercio', logueado, comercioRoutes.agregarComercio);
+app.get("/agregarComercio", logueado, comercioRoutes.formAgregarComercio);
+app.post("/agregarComercio", logueado, comercioRoutes.agregarComercio);
 app.get("/menuComercio", logueado, comercioRoutes.menuComercio);
 app.get("/comercio/:id", logueado, comercioRoutes.listarComercioPorId);
- 
-app.get('/logout', usuarioRoutes.logout);
- 
+
+app.get("/logout", usuarioRoutes.logout);
+
 app.listen(port, () => {
-    console.log(`Servidor escuchando en el puerto ${port}`);
-}); 
+  console.log(`Servidor escuchando en el puerto ${port}`);
+});
